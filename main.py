23f618b48895e44711b5db24f193596eaa0eb187
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2023-05-31 00:10:34 -0300

    add file read feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 from antlr4 import *
 from gen.tweets_grammarLexer import tweets_grammarLexer
 from gen.tweets_grammarParser import tweets_grammarParser
@@ -22,12 +23,24 @@ def processar_entrada():
     resultado_texto.delete("1.0", "end")  # Limpa o widget de texto
     resultado_texto.insert("1.0", resultado)
 
+def carregar_arquivo():
+    arquivo = tk.filedialog.askopenfilename(filetypes=[("Arquivos de Texto", "*.txt")])
+    if arquivo:
+        with open(arquivo, "r") as file:
+            texto = file.read()
+        entrada_texto.delete("1.0", "end")  # Limpa o widget de texto
+        entrada_texto.insert("1.0", texto)
+
 janela = tk.Tk()
 janela.title("ANTLR4 TWEETS")
 
 # Cria a caixa de texto de entrada
 entrada_texto = tk.Text(janela, height=10, width=50)
 entrada_texto.pack()
+
+# Cria o botão de carregar arquivo
+botao_carregar = tk.Button(janela, text="Carregar Arquivo", command=carregar_arquivo)
+botao_carregar.pack()
 
 # Cria o botão de processar
 botao_processar = tk.Button(janela, text="Processar", command=processar_entrada)
